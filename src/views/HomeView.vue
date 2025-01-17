4b0dfeffc1b44ca0a0fa497f02d2cfafc3726a4d
wine <eithinzarphyo430@gmail.com>
2025-01-17 17:51:38 +0630

    messages

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+  
+  
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let posts = ref([]);
+    let errors=ref("")
+    let load = async () => {
+      try {
+        let response = await fetch("http://localhost:3000/postsmm");
+        if (response.status === 404) {
+          throw new Error("Not Found URL.");
+        }
+        let datas=await response.json()
+        posts.value=datas
+       
+      } catch (err) {
+        err.value=err.message
+      }
+    };
+    load();
+    return { posts,errors };
+  },
+};
+</script>
